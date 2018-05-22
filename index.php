c52ec0f0194ab441d64fda1ac0ee07bac03f80d4
anatoliystolyarov <39309267+anatoliystolyarov@users.noreply.github.com>
2018-05-22 18:55:49 +0300

    First Name>> First

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <form action="adduser.php" method="POST">
 	<?php
 		#echocomment
-		echo "Full Name <br><br>";
+		echo "Full your Name<br><br>";
 		echo "<input type='text' name='firstname'><br>";
 		echo "<br>Second Name <br><br>";
 		echo "<input type='text' name='secondname'><br>";
