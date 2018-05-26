fdc2c488da34123bd991b3b19cf0751c73111a0e
anatoliystolyarov <39309267+anatoliystolyarov@users.noreply.github.com>
2018-05-26 12:10:03 +0300

    add new row

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <section id="content">
 <form action="adduser.php" method="POST">
 	<?php
+		echo "dfs"
  		echo "First Name <br><br>";
 		echo "<input type='text' name='firstname'><br>";
 		echo "<br>Second Name <br><br>";
