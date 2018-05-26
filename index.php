8a0d8733016f42edb4d2e9d4e3fb964a2dbd168f
anatoliystolyarov <39309267+anatoliystolyarov@users.noreply.github.com>
2018-05-26 12:05:57 +0300

    add new row

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
 <section id="content">
 <form action="adduser.php" method="POST">
 	<?php
- 		echo "bla"
-		echo "First Name <br><br>";
+ 		echo "First Name <br><br>";
 		echo "<input type='text' name='firstname'><br>";
 		echo "<br>Second Name <br><br>";
 		echo "<input type='text' name='secondname'><br>";
